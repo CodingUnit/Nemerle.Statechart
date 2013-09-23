7776a8e76d9cde13fd20e8b812d3b785c027fa9d
codingunit <codingunit@gmail.com>
2013-09-24 01:40:44 +0400

    add release solution, move some projects, remove unused files, and fix ambiguity in generation

---
+++ Tests/testsuite/run_release.bat
@@ -0,0 +1,27 @@
+@del tests\ /Q /F
+
+
+
+@copy "..\..\Lib\bin\debug\Nemerle.Statechart.Lib.dll" tests\ 
+
+
+
+
+@copy "c:\Program Files\Nemerle\Net-4.0\Nemerle.dll" tests\
+
+
+
+
+
+@copy "..\..\bin\debug\Nemerle.Statechart.dll" tests\ 
+
+
+
+@copy "..\fsmtest\bin\debug\fsmtest.exe" tests\ 
+
+
+
+
+"c:\Program Files\Nemerle\Net-4.0\Nemerle.Compiler.Test.exe" -ref:"..\..\bin\debug\Nemerle.Statechart.dll" -ref:"..\..\Lib\bin\Debug\Nemerle.Statechart.Lib.dll" -ref:"..\fsmtest\bin\debug\fsmtest.exe" -output:tests\ *.n
+pause
+
