dd637a974c48ac11464e8d652fad5a4209b9db3f
codingunit <codingunit@gmail.com>
2013-09-29 01:12:59 +0400

    project fix changes

--- Tests/testsuite/run_release.bat
+++ Tests/testsuite/run_release.bat
@@ -1,10 +1,11 @@
-@del tests\ /Q /F
-@copy "..\..\Lib\bin\debug\Nemerle.Statechart.Lib.dll" tests\ 
-@copy "c:\Program Files\Nemerle\Net-4.0\Nemerle.dll" tests\
-@copy "..\..\bin\debug\Nemerle.Statechart.dll" tests\ 
-@copy "..\..\Nemerle.Statechart.Runtime\bin\debug\Nemerle.Statechart.Runtime.dll" tests\ 
-@copy "..\fsmtest\bin\debug\fsmtest.exe" tests\ 
+@echo off
+mkdir tests\
+del tests\ /Q /F
+copy "..\..\Lib\bin\Release\Nemerle.Statechart.Lib.dll" tests\ 
+copy "c:\Program Files\Nemerle\Net-4.0\Nemerle.dll" tests\
+copy "c:\Program Files\Nemerle\Net-4.0\Nemerle.Peg.dll" tests\
+copy "..\..\bin\Release\Nemerle.Statechart.dll" tests\ 
+copy "..\..\Nemerle.Statechart.Runtime\bin\Release\Nemerle.Statechart.Runtime.dll" tests\ 
+copy "..\fsmtest\bin\Release\fsmtest.exe" tests\ 
 "c:\Program Files\Nemerle\Net-4.0\Nemerle.Compiler.Test.exe" -ref:"tests\Nemerle.Statechart.dll" -ref:"tests\Nemerle.Statechart.Lib.dll" -ref:"tests\fsmtest.exe" -ref:"tests\Nemerle.Statechart.Runtime.dll" -output:tests\ positive\*.n negative\*.n
 pause
-
-
