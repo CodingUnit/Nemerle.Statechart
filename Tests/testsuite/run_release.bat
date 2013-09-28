433017445e0870ee74ffb5d804f13bc18d18439b
codingunit <codingunit@gmail.com>
2013-09-28 19:45:36 +0400

    fix run_release

--- Tests/testsuite/run_release.bat
+++ Tests/testsuite/run_release.bat
@@ -1,35 +1,10 @@
 @del tests\ /Q /F
-
-
-
-
 @copy "..\..\Lib\bin\debug\Nemerle.Statechart.Lib.dll" tests\ 
-
-
-
-
-
 @copy "c:\Program Files\Nemerle\Net-4.0\Nemerle.dll" tests\
-
-
-
-
-
-
 @copy "..\..\bin\debug\Nemerle.Statechart.dll" tests\ 
-
 @copy "..\..\Nemerle.Statechart.Runtime\bin\debug\Nemerle.Statechart.Runtime.dll" tests\ 
-
-
-
-
 @copy "..\fsmtest\bin\debug\fsmtest.exe" tests\ 
-
-
-
-
-
-"c:\Program Files\Nemerle\Net-4.0\Nemerle.Compiler.Test.exe" -ref:"tests\Nemerle.Statechart.dll" -ref:"tests\Nemerle.Statechart.Lib.dll" -ref:"tests\fsmtest.exe" -ref:"tests\Nemerle.Statechart.Runtime.dll" -output:tests\ *.n
+"c:\Program Files\Nemerle\Net-4.0\Nemerle.Compiler.Test.exe" -ref:"tests\Nemerle.Statechart.dll" -ref:"tests\Nemerle.Statechart.Lib.dll" -ref:"tests\fsmtest.exe" -ref:"tests\Nemerle.Statechart.Runtime.dll" -output:tests\ positive\*.n negative\*.n
 pause
 
 
