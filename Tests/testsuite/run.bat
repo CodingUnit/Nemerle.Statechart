c567869e3d7f8d79b57f90359e49f1144e1a710d
codingunit <codingunit@gmail.com>
2018-01-31 18:24:17 +0300

    Lib перенесено в CommonLib.Macros, небольшие починки, взята последняя версия

--- Tests/testsuite/run.bat
+++ Tests/testsuite/run.bat
@@ -1,10 +1,17 @@
 @echo off
+
 mkdir tests\
+
 del tests\ /Q /F
-copy "..\..\Lib\bin\debug\Nemerle.Statechart.Lib.dll" tests\ 
+
+copy "..\..\CommonLib.Macros\bin\debug\CommonLib.Macros.dll" tests\ 
+
 copy "c:\Program Files\Nemerle\Net-4.0\Nemerle.dll" tests\
-copy "..\..\Nemerle.Statechart.Runtime\bin\debug\Nemerle.Statechart.Runtime.dll" tests\ 
+
+copy "..\..\Nemerle.Statechart.Runtime\bin\debug\*.dll" tests\ 
+
 copy "..\fsmtest\bin\debug\fsmtest.exe" tests\ 
-"c:\Program Files\Nemerle\Net-4.0\Nemerle.Compiler.Test.exe" -ref:"..\..\bin\debug\Nemerle.Statechart.dll" -ref:"..\..\Lib\bin\Debug\Nemerle.Statechart.Lib.dll" -ref:..\..\Nemerle.Statechart.Runtime\bin\debug\Nemerle.Statechart.Runtime.dll -ref:"..\fsmtest\bin\debug\fsmtest.exe" -output:tests\ positive\*.n negative\*.n
+
+"c:\Program Files\Nemerle\Net-4.0\Nemerle.Compiler.Test.exe" -ref:"..\..\bin\debug\Nemerle.Statechart.dll" -ref:"tests\CommonLib.Macros.dll" -ref:"tests\System.Reactive.Linq.dll" -ref:"tests\System.Reactive.Core.dll" -ref:..\..\Nemerle.Statechart.Runtime\bin\debug\Nemerle.Statechart.Runtime.dll -ref:"..\fsmtest\bin\debug\fsmtest.exe" -output:tests\ positive\*.n negative\*.n
 pause
 
--- Tests/testsuite/run_release.bat
+++ Tests/testsuite/run_release.bat
@@ -7,5 +7,5 @@ copy "c:\Program Files\Nemerle\Net-4.0\Nemerle.Peg.dll" tests\
 copy "..\..\bin\Release\Nemerle.Statechart.dll" tests\ 
 copy "..\..\Nemerle.Statechart.Runtime\bin\Release\Nemerle.Statechart.Runtime.dll" tests\ 
 copy "..\fsmtest\bin\Release\fsmtest.exe" tests\ 
-"c:\Program Files\Nemerle\Net-4.0\Nemerle.Compiler.Test.exe" -ref:"tests\Nemerle.Statechart.dll" -ref:"tests\Nemerle.Statechart.Lib.dll" -ref:"tests\fsmtest.exe" -ref:"tests\Nemerle.Statechart.Runtime.dll" -output:tests\ positive\*.n negative\*.n
+"c:\Program Files\Nemerle\Net-4.0\Nemerle.Compiler.Test.exe" -ref:"tests\Nemerle.Statechart.dll" -ref:"tests\Nemerle.Statechart.Lib.dll" -ref:"tests\fsmtest.exe" -ref:"tests\Nemerle.Statechart.Runtime.dll" -ref:"tests\System.Reactive.Linq.dll" -ref:"tests\System.Reactive.Core.dll" -output:tests\ positive\*.n negative\*.n
 pause
