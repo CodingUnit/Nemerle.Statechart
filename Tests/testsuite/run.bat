de2ac01849bc486083353e60e4e56cf276d2ee1b
codingunit <codingunit@gmail.com>
2018-02-04 22:52:17 +0300

    исправлены несколько тестов, починены проекты

--- Tests/testsuite/run.bat
+++ Tests/testsuite/run.bat
@@ -3,15 +3,17 @@
 mkdir tests\
 
 del tests\ /Q /F
+del *.txt
 
-copy "..\..\CommonLib.Macros\bin\debug\CommonLib.Macros.dll" tests\ 
 
 copy "c:\Program Files\Nemerle\Net-4.0\Nemerle.dll" tests\
 
 copy "..\..\Nemerle.Statechart.Runtime\bin\debug\*.dll" tests\ 
 
+copy "..\..\CommonLib.Macros\bin\debug\CommonLib.Macros.dll" tests\ 
+
 copy "..\fsmtest\bin\debug\fsmtest.exe" tests\ 
 
-"c:\Program Files\Nemerle\Net-4.0\Nemerle.Compiler.Test.exe" -ref:"..\..\bin\debug\Nemerle.Statechart.dll" -ref:"tests\CommonLib.Macros.dll" -ref:"tests\System.Reactive.Linq.dll" -ref:"tests\System.Reactive.Core.dll" -ref:..\..\Nemerle.Statechart.Runtime\bin\debug\Nemerle.Statechart.Runtime.dll -ref:"..\fsmtest\bin\debug\fsmtest.exe" -output:tests\ positive\*.n negative\*.n
+"c:\Program Files\Nemerle\Net-4.0\Nemerle.Compiler.Test.exe" -ref:"..\..\bin\debug\Nemerle.Statechart.dll" -ref:"tests\CommonLib.Macros.dll" -ref:"tests\System.Reactive.Linq.dll" -ref:"tests\System.Reactive.Core.dll" -ref:"tests\System.Reactive.Interfaces.dll" -ref:"tests\Nemerle.Statechart.Runtime.dll" -ref:"..\fsmtest\bin\debug\fsmtest.exe" -output:tests\ positive\*.n negative\*.n
 pause
 
